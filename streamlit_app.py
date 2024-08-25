7dd84e898f5cc32eeff7d68f67e55b62aa01e101
Jayaprakash <Jaybigdataiscool@gmail.com>
2024-08-25 13:49:19 -0500

    Updating project with latest changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,11 @@
-# import streamlit as st
-
-# # Set the page configuration
-# st.set_page_config(page_title="AlertMinds")
-
-# # Automatically redirect to /Home
-# st.markdown("""
-#     <meta http-equiv="refresh" content="0; url=/Home">
-# """, unsafe_allow_html=True)
-
-
 import streamlit as st
 
 # Set the page configuration
-st.set_page_config(page_title="AlertMinds", layout="wide")
+st.set_page_config(page_title="AlertMinds")
 
-# Check if the current URL path is root and redirect to /Home
-if st.experimental_get_query_params().get("page", [""])[0] == "":
-    st.experimental_set_query_params(page="Home")
-    st.experimental_rerun()
-else:
-    st.markdown("""
-        <meta http-equiv="refresh" content="0; url=/Home">
-    """, unsafe_allow_html=True)
+# Automatically redirect to /Home
+st.markdown("""
+    <meta http-equiv="refresh" content="0; url=/Home">
+""", unsafe_allow_html=True)
+
+
